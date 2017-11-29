7ca374f240d3e5b9d75f5bee195808938c06a389
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-11-29 21:02:13 +0100

    added authentication

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem "font-awesome-sass"
 gem "simple_form"
 gem "autoprefixer-rails"
 
+# authentication and authorizations
+gem 'devise'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'vehicles#index'
-  resources :offers, only: [:show, :index]
-  resources :vehicles, except: [:destroy]
-  resources :contracts, except: [:destroy]
+  devise_for :users
+  root 'orders#index'
+  resources :order, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
