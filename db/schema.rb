413d2fcec9d7ac371aba3c5e9e9c04a020cffe3c
Mykolas Petrauskas <petrauskas.mykolas@gmail.com>
2017-12-04 01:28:42 +0100

    added devise config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129231508) do
+ActiveRecord::Schema.define(version: 20171203235531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20171129231508) do
   create_table "people", force: :cascade do |t|
     t.string "surName"
     t.string "firstName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
